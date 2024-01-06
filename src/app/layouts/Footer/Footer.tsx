72350e3cd004e83efd4b83d5729c8b69679fb271
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2024-01-06 16:08:16 +0200

    added header and footer skeleton

---
+++ src/app/layouts/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./footer.module.css";
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <span className={styles.paragrapgh}>Powered by</span>
+      <Image width={100} height={50} src="/tmdb-logo.png" alt="tmdb logo" />
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/app/layouts/Header/Header.tsx
@@ -0,0 +1,5 @@
+import styles from "./header.module.css";
+const Header = () => {
+  return <header className={styles.header}>Header</header>;
+};
+export default Header;
