e6da85d0f97e2b88b1b87d531863bc578bfad5c4
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2024-01-08 13:19:25 +0200

    removed Test component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+  // const getCarouselMovies = await foo();
+  // const getMovies = await bar();
   return (
     <main className={styles.container}>
       <div className={styles.hero}>Carouserl Headers Buttons</div>
